3050c550b7f497f49fc43ff5b0f54cadfad7c104
Natan <enderbright@gmail.com>
2021-09-29 12:26:00 +0200

    added start, stop, pomoc, koniec "game"

--- Cpp/newfor/main.cpp
+++ Cpp/newfor/main.cpp
@@ -3,14 +3,20 @@
 using namespace std;
 
 int main() {
-    const int n = 5;
+    const int n = 5; //rozmiar tablicy
+    // deklarowanie zmiennej tablicowej
     double tab[n];
 
+    //wprowadzanie z klawiatury wszystkich elementow tablicy
     for(double& element : tab) {
         cout << "element = ";
         cin >> element;
     }
 
+    /*
+     * UWAGA
+     * w petli for powyzej zmienna element oznacza kopie elementu tablicy...
+     */
     double suma = 0;
     for(double element : tab) {
         suma += element;
